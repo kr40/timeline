c56d15b6c8ceb43dddbd5877e4e3795fcbf889ef
Kartik Rao <kr40.git@gmail.com>
2026-07-09 08:36:14 +0530

    feat: add Q&A tab with question submission and parent answering

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { BabyBookTab } from './src/components/BabyBookTab';
 import { ExpandedImageModal } from './src/components/ExpandedImageModal';
 import { HomeTab } from './src/components/HomeTab';
 import { MemoryModal } from './src/components/MemoryModal';
+import { QATab } from './src/components/QATab';
 import { TabBar, TabId } from './src/components/TabBar';
 import { TimelineTab } from './src/components/TimelineTab';
 import { WishesTab } from './src/components/WishesTab';
@@ -171,6 +172,7 @@ const App = () => {
 					/>
 				)}
 				{activeTab === 'wishes'   && <WishesTab />}
+				{activeTab === 'qa'       && <QATab isUnlocked={isUnlocked} />}
 				{activeTab === 'babybook' && <BabyBookTab isUnlocked={isUnlocked} />}
 			</main>
 
